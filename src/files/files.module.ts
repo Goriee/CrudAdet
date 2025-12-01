7663df1bca9d75ad06bc60d706884f65a2dff765
Jerome Devela <jerome.devela.100@gmail.com>
2025-12-02 00:52:04 +0800

    Fix FilesModule dependency: import JwtModule and AuthGuard

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { FilesController } from './files.controller';
 import { FilesService } from './files.service';
 import { DatabaseModule } from '../database/database.module';
+import { AuthGuard } from '../guards/guard';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    DatabaseModule,
+    JwtModule,
+  ],
   controllers: [FilesController],
-  providers: [FilesService],
+  providers: [FilesService, AuthGuard],
   exports: [FilesService],
 })
 export class FilesModule {}
